7dd5f4ae0e001d1fe0a348e49bf2c64a38043904
Tomasz Oskroba <toskroba@sii.pl>
2023-05-28 14:47:36 +0200

    Test implemented

--- src/test/java/UltimateQATest.java
+++ src/test/java/UltimateQATest.java
@@ -14,6 +14,7 @@ public class UltimateQATest extends PageSetup {
     // zwracanie wartosci funkcji - m.in. void
     @Test
     public void testOne() {
+        // Page Object Model / Page Object Pattern
         WebElement buttonUsingId = driver.findElementById("idExample");
         buttonUsingId.click();
         //driver.findElementById("idExample").click();
@@ -23,4 +24,24 @@ public class UltimateQATest extends PageSetup {
         assertEquals("Button success", buttonSuccessText.getText());
     }
 
+    //obsluga wyjatkow
+    @Test
+    public void testTwo() throws InterruptedException {
+        WebElement nameTextField = driver.findElementByName("et_pb_contact_name_0");
+        nameTextField.sendKeys("Tester");
+
+        WebElement emailTextField = driver.findElementByName("et_pb_contact_email_0");
+        emailTextField.sendKeys("tester@tester.com");
+
+        // rodzaje waitow w Selenium: Implicit wait, explicit wait, fluent wait
+        Thread.sleep(2000);
+
+        WebElement emailMeButton = driver.findElementByName("et_builder_submit_button");
+        emailMeButton.click();
+
+        Thread.sleep(2000);
+
+        WebElement thanksText = driver.findElementByClassName("et-pb-contact-message");
+        assertEquals("Thanks for contacting us", thanksText.getText());
+    }
 }
